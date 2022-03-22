11f07c59d453d3568b77369f2828544f6aedd2c4
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-03-22 16:42:20 +0100

    Contrat de Vote

--- 3. Solidity/voting.sol
+++ 3. Solidity/voting.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -31,86 +32,81 @@ contract Voting is Ownable{
     event Voted (address voter, uint proposalId);
 
 
-    WorkflowStatus internal currentWorkflowStatus = WorkflowStatus.RegisteringVoters;
+    WorkflowStatus private currentWorkflowStatus = WorkflowStatus.RegisteringVoters;
     uint private winningProposalId;
-    mapping(address => Voter) voters;
+    mapping(address => Voter) private voters;
     address[] private registeredAddress;
-    Proposal[] private proposals;
+    Proposal[] public proposals;
 
-    function addVoter(address _address) public onlyOwner {
+    function authorize(address _address) external onlyOwner {
         require(currentWorkflowStatus == WorkflowStatus.RegisteringVoters, "Autorize Voter is no loonger possible");
         bool alreadyisRegistered = isRegistered(_address);
         require(!alreadyisRegistered, "Voter already registered");
-        registeredAddress[registeredAddress.length] = _address;
+        registeredAddress.push(_address);
         voters[_address] = Voter(true, false, 0);
         emit VoterRegistered(_address);
     }
 
-    function removeVoter(address _address) public onlyOwner {
+    function removeVoter(address _address) external onlyOwner {
         require(currentWorkflowStatus == WorkflowStatus.RegisteringVoters, "Autorize Voter is no loonger possible");
         voters[_address].isRegistered = false;
     }
 
-    function getWinner() public view returns (Proposal memory) {
+    function getWinner() external view returns (Proposal memory) {
         require(currentWorkflowStatus == WorkflowStatus.VotesTallied, "Votes are not tallied");
         require(winningProposalId >= 0, "No winner can be found");
         return proposals[winningProposalId];
     } 
 
-    function isRegistered(address _address) public view returns (bool) {
+    function isRegistered(address _address) private view returns (bool) {
         return voters[_address].isRegistered;
     }
    
-    function startProposalsRegistration() public onlyOwner {
+    function startProposalsRegistration() external onlyOwner {
         require(currentWorkflowStatus == WorkflowStatus.RegisteringVoters, "Autorize Voter is no loonger possible");
         changeWorkflowStatus(currentWorkflowStatus, WorkflowStatus.ProposalsRegistrationStarted);
     }
 
-    function addProposal(string memory _description) public {
+    function addProposal(string memory _description) external {
+        require(isRegistered(msg.sender), "Unauthorized User");
         require(currentWorkflowStatus == WorkflowStatus.ProposalsRegistrationStarted, "The proposal session has not started");
         proposals.push(Proposal(_description, 0));
+        emit ProposalRegistered(proposals.length);
     }
 
-    function stopProposalsRegistration() public onlyOwner {
+    function stopProposalsRegistration() external onlyOwner {
         require(registeredAddress.length > 0, "Nobody has registered");
         require(currentWorkflowStatus == WorkflowStatus.ProposalsRegistrationStarted, "Proposals Registration is not started");
         changeWorkflowStatus(currentWorkflowStatus, WorkflowStatus.ProposalsRegistrationEnded);
     }
 
-    function startVotingSession() public onlyOwner {
+    function startVotingSession() external onlyOwner {
         require(currentWorkflowStatus == WorkflowStatus.ProposalsRegistrationEnded, "Proposals Registration is not ended");
         changeWorkflowStatus(currentWorkflowStatus, WorkflowStatus.VotingSessionStarted);
     }
 
-    function voteForProposal(uint proposalIndex) public {
+    function voteForProposal(uint proposalId) external {
         require(currentWorkflowStatus == WorkflowStatus.VotingSessionStarted, "Proposals Registration is not started");
-        bool alreadyisRegistered = isRegistered(msg.sender);
-        require(alreadyisRegistered, "Voter not registered");
+        require(isRegistered(msg.sender), "Voter not registered");
         require(!voters[msg.sender].hasVoted, "You have already voted");
-        voters[msg.sender] = Voter(true, true, proposalIndex);
-        emit Voted(msg.sender, proposalIndex);
+        require(proposalId < proposals.length, "Unknow proposalId");
+        voters[msg.sender] = Voter(true, true, proposalId);
+        proposals[proposalId].voteCount = proposals[proposalId].voteCount + 1;
+        emit Voted(msg.sender, proposalId);
     }
 
-    function endVotingSession() public onlyOwner {
+    function endVotingSession() external onlyOwner {
         require(currentWorkflowStatus == WorkflowStatus.VotingSessionStarted, "Proposals Registration is not started");
         changeWorkflowStatus(currentWorkflowStatus, WorkflowStatus.VotingSessionEnded);
     }
 
-    function count() public onlyOwner {
+    function count() external onlyOwner {
         require(currentWorkflowStatus == WorkflowStatus.VotingSessionEnded, "Proposals Registration is not ended");
-        changeWorkflowStatus(currentWorkflowStatus, WorkflowStatus.VotesTallied);
 
-        uint[] memory proposalCount;
-        for (uint index = 0 ; index < registeredAddress.length; index++) {
-            if(voters[registeredAddress[index]].isRegistered) {
-                uint votedProposalId = voters[registeredAddress[index]].votedProposalId;
-                proposalCount[votedProposalId] = proposalCount[votedProposalId] + 1;
-            }
-        }
-
-        int maxIndexFound = findMaxIntoArray(proposalCount);
+        int maxIndexFound = findMaxIntoArray(proposals);
         if(maxIndexFound > 0) {
             winningProposalId = uint(maxIndexFound);
+            changeWorkflowStatus(currentWorkflowStatus, WorkflowStatus.VotesTallied);
         }
     }
 
@@ -119,14 +115,19 @@ contract Voting is Ownable{
         emit WorkflowStatusChange(_previousStatus, _newStatus);
     }
 
-    function findMaxIntoArray(uint[] memory _array) private pure returns(int)  {
+    function findMaxIntoArray(Proposal[] memory _array) private pure returns(int)  {
         uint maxValue = 0;
         int maxIndex = -1;
         for (uint index = 0 ; index < _array.length; index++) {
-            if(_array[index] > maxValue) {
-                maxValue = _array[index];
+            if(_array[index].voteCount > maxValue) {
+                maxValue = _array[index].voteCount;
+                maxIndex = int(index);
             }
         }
         return maxIndex;
+    }
+
+    function getVote(address _address) external view returns(uint){
+        return voters[_address].votedProposalId;
     }
 }
