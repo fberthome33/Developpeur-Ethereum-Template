db6cef5ccbab8d8e801990b18020a4b5f5ef264b
Fabien Berthomé <fabien.berthome.ext@veolia.com>
2022-04-22 08:47:07 +0200

    Projet 3 - add voting

--- 6. Dapp/voting/client/src/App.js
+++ 6. Dapp/voting/client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Voting from "./contracts/Voting.json";
 import getWeb3 from "./getWeb3";
 import Button from 'react-bootstrap/Button';
